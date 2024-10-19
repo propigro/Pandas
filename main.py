b9567e85433e10eaabf18572b784725e29250308
Lazy Programmer <propigro@gmail.com>
2024-10-19 16:05:16 +0500

    Versiya 1.0

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+teachers = pd.DataFrame({
+    "F.I.Sh": ["Xalilov. D. A", "Jalilov .M", "Araboyev . A", "Aloydinov.M", "Qadamova. Z","Jorayeva. G","Xontorayev. S", "Abdullayeva. M", "Turdimatov.M" ],
+    "Fan": ["Suniy intlekt asoslari","Kompyuterni tashkil etish","Kibrxavsizlik asoslari", "Kompyuterni tashkil etish", "Suniy intlekt asoslari", "Eliktronika sixemalar", "Malumotlar tuz va algo", "Malumotlar tuz va algo", "Kibrxavsizlik asoslari"],
+    "Turi": ["Maruza", "Maruza", "Amaliy","Amaliy","Amaliy","Amaliy, Maruza","Maruza", "Amaliy", "Maruza" ]           
+})
+print(teachers)
+teachers.to_excel("teachers.xlsx")
